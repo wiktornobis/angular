a5b3c929fbbdad436c19a4566d7a8ffa48dd0b62
Wiktor Nobis <nobis171@wp.pl>
2022-08-09 11:38:36 +0200

    change car cards on angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarCardComponent } from './car-card/car-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarCardComponent
   ],
   imports: [
     BrowserModule
